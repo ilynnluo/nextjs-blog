f629f719b21c53b5af23bfc0056c78472178645d
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2023-07-02 09:55:43 -0400

    + add MainList UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Layout from "./components/layout"
+import MainList from "./components/mainList/MainList"
 import SideMenu from "./components/sideMenu/SideMenu"
 import Topbar from "./components/topbar/Topbar"
 
@@ -6,8 +7,13 @@ export default function Home() {
   return (
     <Layout>
       <Topbar />
-      <main className="max-w-7xl mx-auto pt-6">
-        <SideMenu />
+      <main className="grid grid-cols-5 max-w-7xl mx-auto pt-6">
+        <div className="col-span-5 sm:col-span-1">
+          <SideMenu />
+        </div>
+        <div className="col-span-5 sm:col-span-4">
+          <MainList />
+        </div>
       </main>
     </Layout>
   )
