18c40db06778c2a4d5f58244405b8f358f25969b
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2023-07-03 11:47:46 -0400

    + add DetailPage UI and > modify Layout structure

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,3 +1,23 @@
-export default function Layout({children}: {children: React.ReactNode}) {
-  return <div>{children}</div>
+import Topbar from "./topbar/Topbar"
+import SideMenu from "./sideMenu/SideMenu"
+import MainList from "./mainList/MainList"
+import DetailPage from "./detailPage/detailPage"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <Topbar />
+      <div className="grid grid-cols-5 max-w-7xl mx-auto pt-6">
+        <div className="col-span-5 sm:col-span-1">
+          <SideMenu />
+        </div>
+        <div className="col-span-5 sm:col-span-4">
+          <main>
+            <MainList />
+            <DetailPage />
+          </main>
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ app/posts/post.tsx
@@ -0,0 +1,7 @@
+import DetailPage from "../components/detailPage/detailPage";
+
+export default function Post() {
+  return (
+    <DetailPage />
+  )
+}
